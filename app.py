e50406ace56d892982d53c9635a5b36f6f2aef58
sachin14596 <sachin140596@gmail.com>
2025-07-27 14:53:15 +0100

    Data Ingestion Updated

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ import os
 import sys
 import boto3
 import streamlit as streamlit
+
+from langchain_community.embeddings import BedrockEmbeddings
+from langchain.llms.bedrock import Bedrock
+
+from langchain.prompts import PromptTemplate
+from langchain.chains import retrieval_qa
+from langchain.vectorstores import FAISS
